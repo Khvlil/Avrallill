47ba0757c73d80d444f60f61d6a5a287d1fc4489
Khvlil <ilcielosenzastelle7@gmail.com>
2023-12-11 13:25:49 +0100

    Test Vercel add correct code

--- Portfolio/Core/views.py
+++ Portfolio/Core/views.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 def home(request):
     try:
-        projects = Post.objects.filter(project_type='web')
+        projects = Post.objects.filter(project_type="web")
     except Post.DoesNotExist:
         projects = None
 
@@ -28,10 +28,18 @@ def home(request):
     paginator = Paginator(projects, 3)
     per_page = request.GET.get("page")
     recommendation = paginator.get_page(per_page)
-    load_type = 'web'
-    version = 'mobile'
+    load_type = "web"
+    version = "mobile"
 
-    return render(request, "LandingPage/Home.html", context={"projects": recommendation, "load_type": load_type, 'version': version})
+    return render(
+        request,
+        "LandingPage/Home.html",
+        context={
+            "projects": recommendation,
+            "load_type": load_type,
+            "version": version,
+        },
+    )
 
 
 def show_project(request, project_name):
@@ -47,23 +55,28 @@ def show_project(request, project_name):
         except Post.DoesNotExist:
             raise Http404("Project not found")
 
-        designs = [design for design in Design.objects.filter(
-            design__project=project_name)]
+        designs = [
+            design for design in Design.objects.filter(design__project=project_name)
+        ]
 
-        works_without_current_project = [
-            work for work in works if work != project]
+        works_without_current_project = [work for work in works if work != project]
 
-        paginator = Paginator(random.sample(
-            works_without_current_project, k=3), 3)
+        if len(works_without_current_project) < 3:
+            # If there are fewer than 3 works, use the entire list
+            recommendation = works_without_current_project
+        else:
+            # Sample 3 works
+            recommendation = random.sample(works_without_current_project, k=3)
 
+        paginator = Paginator(recommendation, 3)
         per_page = request.GET.get("page")
-        recommendation = paginator.get_page(per_page)
+        recommendation_page = paginator.get_page(per_page)
 
         project_context = {
             "project": project,
             "works": works,
             "designs": designs,
-            "recommendation": recommendation,
+            "recommendation": recommendation_page,
         }
 
         # Store the computed context in the cache for 1 hour (you can adjust the timeout)
@@ -71,19 +84,19 @@ def show_project(request, project_name):
 
         # Use the computed context
         context = project_context
-        print('# Use the DB context')
+        print("# Use the DB context")
 
     else:
         # Use the cached context
         context = cached_project_context
-        print('# Use the cached context')
+        print("# Use the cached context")
 
     return render(request, "Project.html", context)
 
 
 def get_project_types(request, type_):
     # Try to get projects from cache
-    cached_project_type_ = cache.get(f'project_type_{type_}')
+    cached_project_type_ = cache.get(f"project_type_{type_}")
 
     if cached_project_type_ is not None:
         # If cached data is available, use it
@@ -96,7 +109,7 @@ def get_project_types(request, type_):
             projects = None
 
         # Store fetched projects in the cache for future use
-        cache.set(f'project_type_{type_}', projects)
+        cache.set(f"project_type_{type_}", projects)
 
     # Paginate the projects
     paginator = Paginator(projects, 3)
@@ -114,7 +127,7 @@ def get_version(request, version):
 
     if not cached_version:
         # If not in cache, compute the context
-        version_context = {'version': version}
+        version_context = {"version": version}
 
         # Store the computed context in the cache for 1 hour
         cache.set(f"version_{version}", version_context, timeout=3600)
@@ -132,52 +145,50 @@ def get_version(request, version):
 def contact_us(request):
     email_send = False
     if request.htmx:
-
-        if request.method == 'POST':
-            print('HTMX')
+        if request.method == "POST":
+            print("HTMX")
             contact_form = ContactForm(request.POST)
             if contact_form.is_valid():
                 contact_form.save()
-                email = contact_form['email'].value()
-                client_subject = contact_form['subject'].value()
-                client_message = contact_form['message'].value()
+                email = contact_form["email"].value()
+                client_subject = contact_form["subject"].value()
+                client_message = contact_form["message"].value()
                 website = request.path
                 print(email)
 
                 email_content(email, client_subject, client_message, website)
 
                 email_send = True
-                print('Everything is correct')
+                print("Everything is correct")
                 if email_send:
-                    messages.success(
-                        request, 'The post has been created successfully.')
+                    messages.success(request, "The post has been created successfully.")
 
-                    return redirect('Home')
-            return render(request, "Contact_Us.html", {'form': contact_form})
+                    return redirect("Home")
+            return render(request, "Contact_Us.html", {"form": contact_form})
         else:
             messages.warning(
-                request, 'Holly molly 😱, something goes wrong ... please re submit your GOGO.')
+                request,
+                "Holly molly 😱, something goes wrong ... please re submit your GOGO.")
             contact_form = ContactForm()
 
-            print('HTMX None')
+            print("HTMX None")
 
-            return render(request, "Contact_Us.html", {'form': contact_form})
+            return render(request, "Contact_Us.html", {"form": contact_form})
 
     return render(request, "Base.html")
 
 
 def email_content(email, client_subject, client_message, website):
-    email_extract = email.split('@', 1)[0]
-    subject = 'Thank You for Contacting Avrallill 🚀'
+    email_extract = email.split("@", 1)[0]
+    subject = "Thank You for Contacting Avrallill "
 
     context = {
-        'email_extract': email_extract,
-        'client_subject': client_subject,
-        'client_message': client_message
+        "email_extract": email_extract,
+        "client_subject": client_subject,
+        "client_message": client_message,
     }
 
-    html_message = render_to_string(
-        'Components/Letter.html', context)
+    html_message = render_to_string("Components/Letter.html", context)
 
     plain_message = strip_tags(html_message)
 
@@ -186,9 +197,9 @@ def email_content(email, client_subject, client_message, website):
         plain_message,
         f"Welcome <{settings.EMAIL_HOST_USER}>",
         [email],
-        html_message=html_message
+        html_message=html_message,
     )
 
 
 def loader(request):
-    return render(request,'Components/Loader.html')
+    return render(request, "Components/Loader.html")
