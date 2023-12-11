a575e3ae76c48a68fdd6d054ac64dc34d4cf5044
Khvlil <ilcielosenzastelle7@gmail.com>
2023-12-11 02:59:49 +0100

    Test Vercel add Postgres add psycopg2 --+

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 MEDIA_DIR = BASE_DIR / "media"
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = "/media/"
--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Core.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
